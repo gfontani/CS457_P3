1b20cd4376fe9fb30b54ec991b66f0a6a10c3e57
Chad <ceitel@live.com>
2016-12-04 23:38:45 -0700

    OSPF WORKING!

--- ospf.cpp
+++ ospf.cpp
@@ -22,6 +22,7 @@ int moveLeastCost(vector<vector<int>> * tentative, vector<vector<int>> * confirm
 
 		for(int i=1; i<(int)tentative->size(); i++){
 			if((*tentative)[i][1]<min_cost){
+				index = i;
 				min_router = (*tentative)[i][0];
 				min_cost = (*tentative)[i][1];
 				min_next = (*tentative)[i][2];		
@@ -66,21 +67,27 @@ bool not_in_confirmed(vector<int> * temp, vector<vector<int>> * confirmed){
 
 bool update_tentative(vector<int> * temp, vector<vector<int>> * tentative){
 	for(int i=0;i<(int)tentative->size(); i++){
-		if((*tentative)[i][0] == (*temp)[0] && (*tentative)[i][1] <= (*temp)[1])
-			return false;
+		if((*tentative)[i][0] == (*temp)[0]){
+			if((*tentative)[i][1] <= (*temp)[1]){
+				return false;
+			}
+			else{
+				tentative->erase(tentative->begin()+i);
+			}
+		}
 	}
 	tentative->push_back(*temp);
 	return true;
 }
 
 
-int inspect(int index, vector<vector<int>> * confirmed, vector<vector<int>> * tentative){
+int inspect(int index, vector<vector<int>> * confirmed, vector<vector<int>> * tentative, int myRouterNum){
 	int router_to_inspect = (*confirmed)[index][0];
 	int cost_to_add = (*confirmed)[index][1];
 	int next_hop = (*confirmed)[index][2];
 
 	for(int i=0; i<(int)allNeighborWeights[router_to_inspect].size(); i++){
-		if((int)allNeighborWeights[router_to_inspect][i]>=0){
+		if(i!= myRouterNum && (int)allNeighborWeights[router_to_inspect][i]>=0){
 				vector<int> temp;
 				temp.push_back(i);
 				temp.push_back(allNeighborWeights[router_to_inspect][i]+ cost_to_add);
@@ -112,12 +119,19 @@ int ospf(int myRouterNum){
 
 	while(tentative_list.size()>0){
 		int index = moveLeastCost(&tentative_list, &confirmed_list);
-		inspect(index, &confirmed_list, &tentative_list);
+		inspect(index, &confirmed_list, &tentative_list, myRouterNum);
 	}
 
 	printf("router %d has reached end of ospf\n",myRouterNum);
-	//routingTable = confirmed_list;
-	//printRoutingTableToFile();
+	routingTable = confirmed_list;
+
+	cout<<"Routing table: \ndest\tweight\tnextHop\n";
+	for(unsigned int i = 0; i < routingTable.size(); i++){
+		for(unsigned int j = 0; j < routingTable[i].size(); j++){
+			cout<<routingTable[i][j]<<"\t";
+		}
+		cout<<"\n";
+	}
 	return 0;
 }
 
